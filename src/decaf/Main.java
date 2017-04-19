f1cedbf11eb864ea99ad2f15ecfa819057ab88a9
UniCEUB <uniceub@localhost.localdomain>
2017-04-19 10:53:03 -0300

    Import do projeto

--- src/decaf/Main.java
+++ src/decaf/Main.java
@@ -68,7 +68,7 @@ class Main {
 		        		done = true;
         			} catch(Exception e) {
         	        	// print the error:
-        	            System.out.println(CLI.infile+" "+e+" em: "token.getText());
+        	            System.out.println(CLI.infile+" "+e+" em: "+text);
         	            lexer.skip();
         	        }
         		}
